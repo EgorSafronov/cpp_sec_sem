59470f6978760caf533e7a250971b514186a85f8
EgorSafronov <safronov.egor.31@gmail.com>
2016-10-12 19:38:34 +0300

    Update lazy_string.h

--- task6/lazy_string.h
+++ task6/lazy_string.h
@@ -12,6 +12,8 @@
 #include <string>
 #include <memory>
 
+using namespace std;
+
 class lazy_string {
     struct lazy_char {
         friend class lazy_string;
@@ -27,15 +29,15 @@ class lazy_string {
     
 private:
     size_t beg,len;
-    std::shared_ptr<std::string> sh_s;
-    lazy_string(std::shared_ptr<std::string> sh, size_t begin, size_t len);
+    shared_ptr<string> sh_s;
+    lazy_string(shared_ptr<string> sh, size_t begin, size_t len);
     
 public:
     //Create new empty lazy_string
     lazy_string();
    
-    //Create new lazy_string from std::string
-    lazy_string(std::string &s);
+    //Create new lazy_string from string
+    lazy_string(string &s);
     
     //Return length of lazy_string
     size_t size() const;
@@ -51,18 +53,18 @@ public:
     char operator[](size_t pos) const;
     lazy_char operator[](size_t pos);
     
-    //Read lazy_string from std::istream
-    friend std::istream &operator >> (std::istream &input, lazy_string &l_s);
+    //Read lazy_string from istream
+    friend istream &operator >> (istream &input, lazy_string &l_s);
     
-    //Write lazy_string to std::ostream
-    friend std::ostream &operator << (std::ostream &output, lazy_string &l_s);
+    //Write lazy_string to ostream
+    friend ostream &operator << (ostream &output, lazy_string &l_s);
     
     //Return a part of lasy_string from "pos" to "pos + len". If this lazy_string is shorter than
     //"pos + len", suffix of lazy_string starting from "pos" will be returned
     lazy_string substr(size_t pos, size_t len);
     
-    //Return std::string copy of lazy_string
-    operator std::string();
+    //Return string copy of lazy_string
+    operator string();
 };
 
 
