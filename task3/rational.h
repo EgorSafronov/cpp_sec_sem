5dcb3f2910fad24acaaad83f96f6a9f6c13f90d8
EgorSafronov <safronov.egor.31@gmail.com>
2016-10-27 12:36:55 +0300

    Update rational.h

--- task3/rational.h
+++ task3/rational.h
@@ -13,16 +13,32 @@ class rational {
 private:
     int num;
     int denom;
+    // Find greatest common divisor
     int gcd(int temp1, int temp2);
     
 public:
+    // Create new rational from integer
     rational(int num);
-    rational(int , int denom);
+    
+    // Create new rational from numerator and denominator
+    rational(int num, int denom);
+    
+    // Do addition of another rational
     rational operator +(rational const &temp) const;
+    
+    // Do substraction of another rational
     rational operator -(rational const &temp) const;
+    
+    // Do multiplication of another rational
     rational operator *(rational const &temp) const;
+    
+    // Do division of another rational
     rational operator /(rational const &temp) const;
+    
+    // Return numerator
     int getNum() const;
+    
+    // Return denomenator
     int getDenom() const;
     
     
